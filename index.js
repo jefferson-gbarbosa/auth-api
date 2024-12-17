a774d631dce209773772bafc354e08de8a82536a
jefferson-gbarbosa <jeffersonqx@gmail.com>
2024-12-17 17:44:30 -0300

    :wastebasket: remove: Removendo arquivos não utilizados do projeto para manter a organização e atualização contínua

--- index.js
+++ index.js
@@ -6,39 +6,11 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors')
 const helmet = require('helmet');
 const swaggerUi = require('swagger-ui-express');
-const swaggerJsdoc = require('swagger-jsdoc');
+const YAML = require('yamljs')
 const authRouter = require('./src/routes/authRoutes.js')
 const app = express();
 
-// Swagger definition
-const swaggerDefinition = {
-    openapi: '3.0.0',
-    info: {
-        title: 'Auth API',
-        version: '1.0.0',
-        description: 'API de autenticação com login, cadastro, recuperação de senha e perfil.',
-    },
-    components: {
-        securitySchemes: {
-          bearerAuth: {
-            type: 'http',
-            scheme: 'bearer',
-            bearerFormat: 'JWT',
-          },
-        },
-    },
-};
-
-// Options for the swagger docs
-const options = {
-    swaggerDefinition,
-    apis: ['./src/controllers/*.js'],
-};
-
-// Initialize swagger-jsdoc
-const swaggerSpec = swaggerJsdoc(options);
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+const swaggerFile = YAML.load('./swagger.yaml')
 
 app.use(express.json());
 app.use(cookieParser());
@@ -50,6 +22,8 @@ app.use(cors({
 app.use(helmet());
 
 app.use("/auth",authRouter)
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
