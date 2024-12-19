c91106f7a64040eb658658fd56e2e4a961b4a2d6
jefferson-gbarbosa <jeffersonqx@gmail.com>
2024-12-19 17:16:37 -0300

    :boom: fix: Revertendo mudanças ineficientes

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const helmet = require('helmet');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs')
 const authRouter = require('./src/routes/authRoutes.js')
-const path = require('path')
 const app = express();
 
 const swaggerFile = YAML.load('./swagger.yaml')
@@ -25,14 +24,6 @@ app.use(helmet());
 app.use("/auth",authRouter)
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
-
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-	});
-}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
