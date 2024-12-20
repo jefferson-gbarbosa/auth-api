3ca2d2757577f90522b6c09d9e760ac2db6a7489
jefferson-gbarbosa <jeffersonqx@gmail.com>
2024-12-20 20:22:40 -0300

    :wrench: chore: configurações de cookie

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -7,7 +7,7 @@ const  generateTokenAndSetCookie = (id,res) =>{
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
 		sameSite: "strict",
-		maxAge: 7 * 24 * 60 * 60 * 1000,
+		maxAge: 60 * 15,
     });
 
     return token;
